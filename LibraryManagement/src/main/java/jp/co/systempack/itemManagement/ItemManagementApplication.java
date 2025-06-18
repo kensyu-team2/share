82d74469911ead3b92871138fbf17bbfe77b0c8b
takeshita0623 <takeshita.yoshihisa@aaa-system.co.jp>
2025-06-18 15:53:26 +0900

    html貸出返却

--- LibraryManagement/src/main/java/jp/co/systempack/itemManagement/ItemManagementApplication.java
+++ LibraryManagement/src/main/java/jp/co/systempack/itemManagement/ItemManagementApplication.java
@@ -2,8 +2,9 @@ package jp.co.systempack.itemManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // この行を追加
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ItemManagementApplication {
 
     public static void main(String[] args) {
