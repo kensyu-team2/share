0bcb1a5e589713c950b00224a2f30375fd68ebf6
koike29 <tomoki.koike@cosmicb.co.jp>
2025-06-19 13:30:51 +0900

    a

--- LibraryManagement/src/main/java/MemberManagement/MemberManagementApplication.java
+++ LibraryManagement/src/main/java/MemberManagement/MemberManagementApplication.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // この行を追加
 
-<<<<<<< HEAD:LibraryManagement/src/main/java/MemberManagement/MemberManagementApplication.java
 @SpringBootApplication
 public class MemberManagementApplication {
-=======
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ItemManagementApplication {
->>>>>>> 82d74469911ead3b92871138fbf17bbfe77b0c8b:LibraryManagement/src/main/java/jp/co/systempack/itemManagement/ItemManagementApplication.java
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         SpringApplication.run(MemberManagementApplication.class, args);
     }
 
+}
 }
